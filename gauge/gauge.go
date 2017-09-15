57573b5d081844cd41a270bb4e353afe4010c306
Diego Garcia <drgarcia1986@gmail.com>
2017-09-15 18:53:13 -0300

    prevent metric errors on registred prometheus gauge twice

--- gauge/gauge.go
+++ gauge/gauge.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -12,6 +13,11 @@ import (
 type Gauge interface {
 	Set(float64) error
 }
+
+var (
+	mu              *sync.Mutex
+	registredGauges map[string]prometheus.Gauge
+)
 
 type PrometheusGauge struct {
 	pg prometheus.Gauge
@@ -22,7 +28,23 @@ func (p *PrometheusGauge) Set(metric float64) error {
 	return nil
 }
 
-func NewPrometheusGauge(namespace, deploy, metricType string) (Gauge, error) {
+func init() {
+	mu = new(sync.Mutex)
+	registredGauges = make(map[string]prometheus.Gauge)
+}
+
+func NewPrometheusGauge(namespace, deploy, metricType string) Gauge {
+	return &PrometheusGauge{pg: getOrCreateGauge(namespace, deploy, metricType)}
+}
+
+func getOrCreateGauge(namespace, deploy, metricType string) prometheus.Gauge {
+	mu.Lock()
+	defer mu.Unlock()
+
+	gId := fmt.Sprintf("%s%s%s", namespace, deploy, metricType)
+	if g, found := registredGauges[gId]; found {
+		return g
+	}
 	g := prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "mitose",
 		Help: "Mitose autoscaller",
@@ -32,12 +54,10 @@ func NewPrometheusGauge(namespace, deploy, metricType string) (Gauge, error) {
 			"metric_type": metricType,
 		},
 	})
-	if err := prometheus.Register(g); err != nil {
-		if _, ok := err.(prometheus.AlreadyRegisteredError); !ok {
-			return nil, err
-		}
-	}
-	return &PrometheusGauge{pg: g}, nil
+	prometheus.Register(g)
+	registredGauges[gId] = g
+
+	return g
 }
 
 func Run() error {
